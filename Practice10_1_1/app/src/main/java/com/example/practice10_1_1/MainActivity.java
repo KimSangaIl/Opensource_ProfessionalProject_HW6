35c0ffeaa1e94bfdc221e05671007992ded0170b
KimSangil <asfff0321@naver.com>
2023-04-12 14:52:39 +0900

    Practice 10-1-1 initial commit

---
+++ Practice10_1_1/app/src/main/java/com/example/practice10_1_1/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.practice10_1_1;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final RadioButton rdoSecond = findViewById(R.id.rdoSecond);
+
+        Button btnNewActivity = (Button) findViewById(R.id.btnNewActivity);
+        btnNewActivity.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent;
+
+                if (rdoSecond.isChecked() == true) intent = new Intent(getApplicationContext(), SecondActivity.class);
+                else intent = new Intent(getApplicationContext(), ThirdActivity.class);
+
+                startActivity(intent);
+            }
+        });
+    }
+}
